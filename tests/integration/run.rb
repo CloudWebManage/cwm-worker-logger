472741fa0ec1091cf6a221e9c922107b32d0c485
Azeem Sajid <azeem.sajid@gmail.com>
2021-01-28 12:01:00 +0500

    Updated integration tests.
    Signed-off-by: Azeem Sajid <azeem.sajid@gmail.com>

--- tests/integration/run.rb
+++ tests/integration/run.rb
@@ -1,20 +1,25 @@
-#!/usr/bin/ruby
-
 # frozen-string-literal: true
 
 require 'net/http'
 
-http = Net::HTTP.new('127.0.0.1', 8500)
-header = { 'Content-Type' => 'application/json' }
+puts 'Starting integration test...'
 
-File.readlines('./tests/integration/logs').each do |line|
+http = Net::HTTP.new('127.0.0.1', 8500)
+header = { 'Content-Type' => 'application/json' }.freeze
+
+File.readlines('./tests/integration/logs').each_with_index do |line, index|
+  print "Sending request # #{index + 1}... "
   req = Net::HTTP::Post.new('/logs', header)
   req.body = line
   http.request(req)
+  puts '[OK]'
 end
 
-# Wait for flush i.e. 10s
+print 'Waiting for flush interval... '
 sleep(10)
+puts '[OK]'
+
+print 'Verifying data from Redis server... '
 
 EXPECTED_OUTPUT = {
   'deploymentid:minio-metrics:docker-compose-http:bytes_in' => '76727',
@@ -27,8 +32,11 @@ EXPECTED_OUTPUT = {
 EXPECTED_OUTPUT.each do |key, value|
   output = `redis-cli GET #{key}`
   output.chomp!
-  if output != value
+  unless output == value
     puts "ERROR: [#{key} != #{value}] Got: #{output}, Expected: #{value}"
     return -1
   end
 end
+
+puts '[OK]'
+puts 'Integration test completed successfully!'
