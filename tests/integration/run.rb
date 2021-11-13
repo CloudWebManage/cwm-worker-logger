9ba5857cdcb8ee387cddf55c26ce6c3876c0d75b
Azeem Sajid <azeem.sajid@gmail.com>
2021-11-13 16:37:22 +0500

    Improve tests.

--- tests/integration/run.rb
+++ tests/integration/run.rb
@@ -2,24 +2,11 @@
 
 require 'net/http'
 
-puts 'Starting integration test...'
-
-http = Net::HTTP.new('127.0.0.1', 8500)
-header = { 'Content-Type' => 'application/json' }.freeze
-
-File.readlines('./tests/integration/logs').each_with_index do |line, index|
-  print "Sending request # #{index + 1}... "
-  req = Net::HTTP::Post.new('/logs', header)
-  req.body = line
-  http.request(req)
-  puts '[OK]'
-end
-
-print 'Waiting for flush interval... '
-sleep(10)
-puts '[OK]'
-
-print 'Verifying data from Redis server... '
+HOST = '127.0.0.1'
+PORT = 8500
+URL_PATH = '/logs'
+LOGS_FILE_PATH = './tests/integration/logs'
+FLUSH_INTERVAL_WAIT_SECS = 10
 
 EXPECTED_OUTPUT = {
   'deploymentid:minio-metrics:docker-compose-http:bytes_in' => '76727',
@@ -28,6 +15,25 @@ EXPECTED_OUTPUT = {
   'deploymentid:minio-metrics:docker-compose-http:num_requests_out' => '7',
   'deploymentid:minio-metrics:docker-compose-http:num_requests_misc' => '5'
 }.freeze
+
+puts "Sending requests to http://#{HOST}:#{PORT}#{URL_PATH}"
+
+http = Net::HTTP.new(HOST, PORT)
+header = { 'Content-Type' => 'application/json' }.freeze
+
+File.readlines(LOGS_FILE_PATH).each_with_index do |line, index|
+  print "Request # #{index + 1}\t"
+  req = Net::HTTP::Post.new(URL_PATH, header)
+  req.body = line
+  http.request(req)
+  puts '[OK]'
+end
+
+print "Waiting for flush interval [#{FLUSH_INTERVAL_WAIT_SECS}s]... "
+sleep(FLUSH_INTERVAL_WAIT_SECS)
+puts '[OK]'
+
+print 'Verifying data from Redis server... '
 
 EXPECTED_OUTPUT.each do |key, value|
   output = `redis-cli GET #{key}`
