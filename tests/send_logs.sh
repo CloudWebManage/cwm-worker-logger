1b0b069cef9ecbc84efcd89cf7b473b0d143e95c
Azeem Sajid <azeem.sajid@gmail.com>
2021-11-15 21:29:41 +0500

    Improve tests. (#11)

--- tests/send_logs.sh
+++ tests/send_logs.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-TARGET="${1}"
+TARGET="$1"
 
-[ "${TARGET}" == "" ] && echo missing TARGET && exit 1
+[[ -z $TARGET ]] && echo 'missing TARGET' && exit 1
 
-echo sending logs to "${TARGET}"
+echo "sending logs to $TARGET"
 
 while true; do
   while read -r line; do
-    if ! curl -sX POST -H "Content-Type: application/json" -d "${line}" "${TARGET}"; then
-      echo failed to post log line to target
+    if ! curl -sX POST -H 'Content-Type: application/json' -d "$line" "$TARGET"; then
+      echo 'failed to post log line to target'
       exit 1
     fi
     sleep 0.1
-  done <tests/logs
+  done <'./tests/logs'
   sleep 0.1
 done
